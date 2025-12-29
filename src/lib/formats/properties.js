a6258eae98b890b6951e00c66eae377c66704763
Diogo Correia <me@diogotc.com>
2025-12-29 02:21:35 +0000

    fix: use properties-file package

--- src/lib/formats/properties.js
+++ src/lib/formats/properties.js
@@ -1,7 +1,12 @@
-//import properties from 'properties';
+import { getProperties } from 'properties-file';
+import { escapeKey, escapeValue } from 'properties-file/escape';
 
-export const fromFileContent = (content) => properties.parse(content);
+export const fromFileContent = (content) => getProperties(content);
 
-export const toFileContent = (content) => properties.stringify(content);
+export const toFileContent = (content) => {
+  return Object.entries(content)
+    .map(([key, value]) => `${escapeKey(key)} = ${escapeValue(value)}\n`)
+    .join("");
+}
 
 export const originalFileName = 'original.properties';
